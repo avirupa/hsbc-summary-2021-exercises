42002f76bd6717a5bca234a2b8ed996e7f3f6e49
avirupa <51082328+avirupa@users.noreply.github.com>
2021-10-05 17:18:35 +0530

    Day1-exercise

---
+++ Submission/CheckChar.java
@@ -0,0 +1,26 @@
+package com.example;
+import java.util.Scanner;
+
+public class CheckChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a character: ");
+		char s=sc.next().charAt(0);
+		checkAlphabet(s);
+
+	}
+	public static void checkAlphabet(char c)
+	{
+		if((c>=97 && c<=122)||(c>=65 && c<=90))
+				{
+					System.out.println("It is an alphabet");
+				}
+		else
+		{
+			System.out.println("Not an alphabet");
+		}
+	}
+
+}
